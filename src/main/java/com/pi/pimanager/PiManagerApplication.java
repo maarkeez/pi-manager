f5dd2b2732cb5d4d979d8ab33502e987e66ec9dc
maarkeez <d.marquez.delgado.91@gmail.com>
2019-11-12 23:00:06 +0100

    Initial commit

---
+++ src/main/java/com/pi/pimanager/PiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pi.pimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/pimanager/PiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.pimanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
